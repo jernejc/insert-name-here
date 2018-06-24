18f19c2da6c8d4e10113622531f52cea1823a862
Jernej <j@eee.si>
2018-06-24 20:43:06 +0200

    cleanup and ga

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
 
-    /***************** Article Class Finder ******************/
-
-    $('.latest-articles').find('img').each(function() {
-        var imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
-        $(this).addClass(imgClass);
-    });
-
     /***************** Like Counter ******************/
 
     $('.count').each(function() {
@@ -31,69 +24,6 @@ $(document).ready(function() {
     $("li a.share-trigger").on("click", function() {
         $('.share-dropdown').toggleClass("is-open");
         event.preventDefault();
-    });
-
-    /***************** Search Component ******************/
-
-    $(".show-search").on("click", function() {
-        $(".search-wrapper").addClass("is-visible");
-    });
-
-    $(".hide-search").on("click", function() {
-        $(".search-wrapper").removeClass("is-visible");
-        $(".search-wrapper input").removeClass("is-selected");
-    });
-
-    $(".search-wrapper input").on("click", function() {
-        $(this).addClass("is-selected");
-    });
-
-    $('.search-wrapper input').keypress(function(e) {
-        if (e.which === 13) { //Enter key pressed
-            window.alert("Ready for implementation.");
-        }
-    });
-
-    /***************** Bar Chart Animation ******************/
-
-    $('.bar').width('0%');
-    $('.bar').waypoint(function() {
-        $('.bar').each(function() {
-            var width = $(this).data("percentage");
-            $(this).animate({
-                width: width
-            }, {
-                duration: 2000,
-                easing: 'easeOutExpo',
-            });
-        });
-    }, {
-        offset: '85%'
-    });
-
-    /***************** Stats Counter ******************/
-
-    var counterZero = '0';
-    $('.stats-number').text(counterZero);
-
-    $('.stats-number').waypoint(function() {
-        $('.stats-number').each(function() {
-            var $this = $(this);
-            $({
-                Counter: 0
-            }).animate({
-                Counter: $this.attr('data-stop')
-            }, {
-                duration: 5000,
-                easing: 'swing',
-                step: function(now) {
-                    $this.text(Math.ceil(now));
-                }
-            });
-        });
-        this.destroy();
-    }, {
-        offset: '75%'
     });
 
     /***************** Smooth Scroll ******************/
@@ -124,49 +54,6 @@ $(document).ready(function() {
         $('.nav-toggle').toggleClass('active');
         $('.navicon').toggleClass('fixed');
         $('.primary-nav-wrapper').toggleClass('open');
-    });
-
-    /***************** Waypoints ******************/
-
-    $('.wp1').waypoint(function() {
-        $('.wp1').addClass('animated fadeInUp');
-    }, {
-        offset: '80%'
-    });
-    $('.wp2').waypoint(function() {
-        $('.wp2').addClass('animated fadeInUp');
-    }, {
-        offset: '95%'
-    });
-    $('.wp3').waypoint(function() {
-        $('.wp3').addClass('animated fadeInUp');
-    }, {
-        offset: '95%'
-    });
-    $('.wp4').waypoint(function() {
-        $('.wp4').addClass('animated fadeInUp');
-    }, {
-        offset: '75%'
-    });
-    $('.wp5').waypoint(function() {
-        $('.wp5').addClass('animated fadeIn');
-    }, {
-        offset: '75%'
-    });
-    $('.wp6').waypoint(function() {
-        $('.wp6').addClass('animated fadeIn');
-    }, {
-        offset: '75%'
-    });
-    $('.wp7').waypoint(function() {
-        $('.wp7').addClass('animated fadeIn');
-    }, {
-        offset: '75%'
-    });
-    $('.wp8').waypoint(function() {
-        $('.wp8').addClass('animated fadeIn');
-    }, {
-        offset: '75%'
     });
 
     /***************** Overlay touch/hover events ******************/
